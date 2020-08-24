c5cb8067f79534145f71d6a7d6ec1d35e6b7e9fe
Kevin Jeffers <jevinkeffers@gmail.com>
2020-08-24 15:52:22 -0400

    Added category functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,34 @@
 'use strict';
 
-
-
-
 const chuckSays = document.getElementById('chuckSays');
+const button = document.querySelector ('button');
+const submitFormButton = document.getElementById('submitForm');
+const defaultCategory = "dev"
 
-function get() {
-    // STEP 1: FETCH THE DATA
-    return fetch("https://api.chucknorris.io/jokes/random?category=dev")
-    .then(function(response) { // STEP 2: Run the json() method from the Resposne
-        return response.json()
-    })
-    .then(function (data) { //STEP 3: Return the data from the response.json() method
-        return data;
-    })
+// function getQuote(category) {}
+const getQuote = (category) => {
+    const url = `https://api.chucknorris.io/jokes/random?category=${category}`;
+
+    get(url).then(function(fetchResponse) {
+        chuckSays.innerHTML = fetchResponse.value
+    });
 }
 
-(function()) {
-    
-}(); // <<<< Anonymous function syntax // Alows us a new scope outside of global scope that is just tied to doing what we want to do right now
+refreshQuote.addEventListener('click', function(e) {
+    e.preventDefault();
+    getQuote(defaultCategory);
+} 
+);
+
+submitFormButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    const userInput = document.getElementById("categoryInput");
+    const category = userInput.value;
+    getQuote(category);
+});
+
+(function() {
+    getQuote(defaultCategory)
+})(); // IIFE
+
+
